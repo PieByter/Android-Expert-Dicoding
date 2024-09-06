fab36d99cc8decf70fa32083a6163bf6e86e524b
Pieter T <xerabellion@gmail.com>
2024-09-06 15:14:07 +0700

    Certificate Pinning

--- core/src/main/java/com/githubproject/core/domain/data/remote/retrofit/ApiConfig.kt
+++ core/src/main/java/com/githubproject/core/domain/data/remote/retrofit/ApiConfig.kt
@@ -1,6 +1,7 @@
 package com.githubproject.core.domain.data.remote.retrofit
 
 import com.githubproject.core.BuildConfig
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -10,6 +11,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
+            val certificatePinner = CertificatePinner.Builder()
+                .add("api.github.com", "sha256/GyhWVHsOXNZc6tGTNd15kXF9YD0kEZaGxYn6MUva5jY=")
+                .add("api.github.com", "sha256/lmo8/KPXoMsxI+J9hY+ibNm2r0IYChmOsF9BxD74PVc=")
+                .build()
+
             val loggingInterceptor = HttpLoggingInterceptor().apply {
                 level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
             }
@@ -25,6 +31,7 @@ class ApiConfig {
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .addInterceptor(authInterceptor)
+                .certificatePinner(certificatePinner)
                 .build()
 
             val retrofit = Retrofit.Builder()
